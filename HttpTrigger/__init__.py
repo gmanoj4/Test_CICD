ceb508d04370f5717e323200bd4f22c61fd02240
Reddy <g-manoj.reddy@atos.net>
2020-09-29 12:01:55 +0530

    code for test function

---
+++ HttpTrigger/__init__.py
@@ -0,0 +1,29 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    test = req.params.get('test')
+    age = req.params.get('age')
+
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+            test = req_body.get('test')
+            age = req_body.get('age')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}, {test}, {age}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
